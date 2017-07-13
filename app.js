0887564375105ef9211cc2f2bf7f1db3593d2860
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-07-12 22:29:12 -0700

    updates from day 3 lab, new question flow using arrays and while loops

--- app.js
+++ app.js
@@ -1,45 +1,70 @@
 "use strict";
 
-alert('Hello! \n Let\'s see how well you know me! \n Please enter \"Yes\" or \"No\" in the response box!');
+var userName = prompt('Welcome to my "About Me" page! \n What is your name?');
+alert('Nice to meet you, ' + userName + '! \n Let\'s see how well you know me! \n Please enter \"Yes\" or \"No\" in the response box!');
 
-var oldestchild = prompt('Am I the oldest child?').toLowerCase();
-if (oldestchild === 'yes' || oldestchild === 'y') {
-    alert('Nope! Actually, I\'m an only child');
+// 5 questions about me with yes or no answers
+var myQuestions = ['Am I the oldest child?', 'Do I have all ten fingerprints?', 'Can I eat a McDonald\'s 20 piece McNugget in one sitting?', 'Did the user think my favorite movie genre was Romance?', 'Do I love my dog?'];
+var myWordAnswers = ['no', 'no', 'yes', 'no', 'yes'];
+var myLetterAnswers = ['n', 'n', 'y', 'n', 'y'];
+var answersCorrect = 0;
+
+for (var i = 0; i < myQuestions.length; i++) {
+  var userResponse = prompt(myQuestions[i].toLowerCase());
+    if (userResponse === myWordAnswers[i] || userResponse === myLetterAnswers[i]) {
+      alert('Correct!');
+      answersCorrect++;
+    } else {
+      alert('Wrong!');
+    }
+    console.log('The user gave the answer ' + userResponse[i] + ' to the question ' + myQuestions[i]);
+};
+
+//a number guessing game where the user has 4 tries
+var myNumber = 13;
+var guessesLeft = 4;
+var userGuess;
+
+while (guessesLeft > 0 && userGuess !== myNumber) {
+userGuess = parseInt(prompt('Guess a number between 10 - 20'));
+  if (userGuess > myNumber) {
+    guessesLeft--;
+    alert('Your guess is too high \n You have ' + guessesLeft + ' guesses remaining!');
+  } else if (userGuess < myNumber) {
+    guessesLeft--;
+    alert('Your guess is too low  \n You have ' + guessesLeft + ' guesses remaining!');
+  } else if (userGuess === myNumber) {
+    alert('Correct answer!');
+    answersCorrect++ ;
   } else {
-    alert('Correct!');
+    alert('Please enter a number');
   };
-    console.log('Did the user think I was the oldest child? ' + oldestchild);
+  if (guessesLeft === 0) {
+    alert('Sorry, you\'re all out of guesses!');
+  };
+}
 
-var fingerprints = prompt('Do I have all ten fingerprints?').toLowerCase();
-if (fingerprints === 'yes' || fingerprints === 'y') {
-  alert('Unfortunately, I only have nine');
-} else {
-  alert('It\'s weird that you knew that...');
-};
-console.log('Did the user think I have all ten fingerprints? ' + fingerprints);
 
-var chicknuggs = prompt('Can I eat a McDonald\'s 20 piece McNugget in one sitting?').toLowerCase();
-if (chicknuggs === 'yes' || chicknuggs === 'y') {
-  alert('I sure can, and I sure will');
-} else {
-  alert('Wrong. Have some faith in me.');
-};
-console.log('Did the user believe in my ability to eat 20 chicken nuggets? ' + chicknuggs);
+//a question with multiple correct answers stored in an array
 
-var moviegenre = prompt('Is Romance my favorite movie genre?').toLowerCase();
-if (moviegenre === 'yes' || moviegenre === 'y') {
-  alert('Nope, take \'The Notebook\' somewhere else.');
-} else {
-  alert('Correct!');
-};
-console.log('Did the user think my favorite movie genre was Romance? ' + moviegenre);
+guessesLeft = 6;
+while (guessesLeft > 0) {
+  console.log(guessesLeft);
+  userGuess = prompt('Guess a color I have dyed my hair?').toLowerCase();
+  var fruit = ['black', 'pink', 'green', 'blue', 'purple', 'red'];
+    if (fruit.includes(userGuess)){
+    guessesLeft = guessesLeft - 6;
+    alert('Correct!');
+      } else {
+      guessesLeft--;
+      alert('Wrong!');
+    };
+    if (guessesLeft === 0) {
+    alert('The colors I have dyed my hair so far are black, pink, green, blue, purple, and red!');
+    };
+  }
 
-var dog = prompt('Do I love my dog?').toLowerCase();
-if (dog === 'yes' || dog === 'y') {
-  alert('Of course I love my dog');
-} else {
-  alert('Wrong, you monster.');
-};
-console.log('Did the use answer the last question yes? ' + dog);
 
-alert('I hope you enjoyed this quiz! Feel free to explore more about me and see extended responses to my answers!');
+
+
+console.log('The user has gotten ' + answersCorrect + ' answers correct');
